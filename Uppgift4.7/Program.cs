e8795ce9d562dcc49b64ed1c425f69ea13576bc8
arun06001 <arun06001@E-PW0234KE>
2023-10-18 13:27:06 +0200

    Add project files.

---
+++ Uppgift4.7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Uppgift4_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande");
+            string meddelande = Console.ReadLine();
+
+            Console.WriteLine("Skriv in ett heltal");
+            int förskjutning = int.Parse(Console.ReadLine());
+
+            for (int i = 0;i < meddelande.Length;i++)
+            {
+                for (int j = 0; j < förskjutning; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
